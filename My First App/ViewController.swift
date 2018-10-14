9f2a57a461b10f918146305195caa300fb6160c2
松田雅海 <matsuda@matsuda.local>
2018-10-14 14:52:44 +0900

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by 松田雅海 on 2018/10/14.
+//  Copyright © 2018年 JohnApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
